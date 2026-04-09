1cce408e9f6bf3e14037a1f5bd451effc195ca7f
Nuño Rodríguez Giovanna Yamile <gnr267@inlumine.ual.es>
2026-04-09 19:56:30 +0200

    Creacion de clase Conexion

---
+++ juegos1000tres-backend/src/main/java/com/juegos1000tres/juegos1000tres_backend/modelo/modelos/Conexion.java
@@ -0,0 +1,6 @@
+package com.juegos1000tres.juegos1000tres_backend.modelo.modelos;
+
+public interface  Conexion {
+    public void conectar();
+    public void desconectar();
+}
--- juegos1000tres-backend/src/main/java/com/juegos1000tres/juegos1000tres_backend/modelo/modelos/Juego.java
+++ juegos1000tres-backend/src/main/java/com/juegos1000tres/juegos1000tres_backend/modelo/modelos/Juego.java
@@ -4,14 +4,18 @@ public abstract class Juego {
 
     private final int numeroJugadores;
     private final boolean necesitaPantalla;
+    private final Conexion conexionJugadores;
+    private final Conexion conexionPantalla;
 
-    protected Juego(int numeroJugadores, boolean necesitaPantalla) {
+    protected Juego(int numeroJugadores, boolean necesitaPantalla, Conexion conexionJugadores, Conexion conexionPantalla) {
         if (numeroJugadores <= 0) {
             throw new IllegalArgumentException("El numero de jugadores debe ser mayor que cero");
         }
 
         this.numeroJugadores = numeroJugadores;
         this.necesitaPantalla = necesitaPantalla;
+        this.conexionJugadores = conexionJugadores;
+        this.conexionPantalla = conexionPantalla;
     }
 
     public int getNumeroJugadores() {
